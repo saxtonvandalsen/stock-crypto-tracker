c925c3e1bd30d5145a65df702d4a0a34add99ebb
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-09-06 09:09:15 -0700

    Added comments for clarity

--- src/components/CryptoForm.js
+++ src/components/CryptoForm.js
@@ -1,17 +1,26 @@
-// 
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchCryptoData } from '../redux/actions';
+// Handling user input of crypto coin symbol and dispatching an action to fetch stock data
+
+import React, { useState } from 'react'; // Hook to manage local component state
+import { useDispatch } from 'react-redux'; // To dispatch actions to the Redux stor
+import { fetchCryptoData } from '../redux/actions'; // Fetching stock data by ticker
 
 function CryptoForm() {
+
+    // To update state when input changes
     const [symbol, setSymbol] = useState('');
+
+    // Accesses dispatch function and used to send actons to Redux store
     const dispatch = useDispatch();
 
+    // Function runs when user presses submit button on form but prevents
+    // the default form behavior (page refresh) and fetchs the stock data
     const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(fetchCryptoData(symbol));
     };
 
+    // JSX to handle the text input field where the user can type in 
+    // the crypto coin symbol and submit
     return (
         <form onSubmit={handleSubmit}>
             <input
@@ -25,4 +34,5 @@ function CryptoForm() {
     );
 }
 
+// Available for import and use in other files
 export default CryptoForm;
--- src/components/StockForm.js
+++ src/components/StockForm.js
@@ -1,16 +1,27 @@
-import React, {useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchStockData } from '../redux/actions';
+// Handling user input of stock ticker and dispatching an action to fetch stock data
+
+import React, {useState } from 'react'; // Hook to manage local component state
+import { useDispatch } from 'react-redux'; // To dispatch actions to the Redux store
+import { fetchStockData } from '../redux/actions'; // Fetching stock data by ticker
+
 
 function StockForm() {
+
+    // To update state when input changes
     const [ticker, setTicker] = useState('');
+
+    // Accesses dispatch function and used to send actons to Redux store
     const dispatch = useDispatch();
 
+    // Function runs when user presses submit button on form but prevents
+    // the default form behavior (page refresh) and fetchs the stock data
     const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(fetchStockData(ticker));
     };
 
+    // JSX to handle the text input field where the user can type in 
+    // the stock ticker and submit
     return (
         <form onSubmit={handleSubmit}>
             <input
@@ -24,4 +35,5 @@ function StockForm() {
     );
 }
 
+// Available for import and use in other files
 export default StockForm;
