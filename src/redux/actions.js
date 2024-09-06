637c713564d060a3f57b3d5d2b82105e3752012c
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-09-06 16:18:48 -0700

    Added comments to redux files

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,15 +1,23 @@
+// Action types for fetching stock and crypto data
 export const FETCH_STOCK_DATA = 'FETCH_STOCK_DATA';
 export const FETCH_CRYPTO_DATA = 'FETCH_CRYPTO_DATA';
 
-// 
+// Action creator for fetching stock data from Company Profile API
 export const fetchStockData = (ticker) => async (dispatch) => {
+    // Asynchronous request to fetch stock data based on provided ticker input
     const response = await fetch(`https://financialmodelingprep.com/api/v3/profile/${ticker}?apiKey=YOUR_API_KEY`);
     const data = await response.json();
+
+    // Dispatch action to update Redux state with fetched stock data
     dispatch({ type: FETCH_STOCK_DATA, payload: data });
 };
 
+// Action creator for fetching cryptocurrency data from Full Crypto Quote API
 export const fetchCryptoData = (symbol) => async (dispatch) => {
+    // Asychronous request to fetch crypto data based on provided symbol input
     const response = await fetch(`https://financialmodelingprep.com/api/v3/quote/${symbol}?apiKey=YOUR_API_KEY`);
     const data = await response.json();
+
+    // Dispatch action to update Redux state with fetched crypto data
     dispatch({ type: FETCH_CRYPTO_DATA, payload: data });
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import reducer from './reducers';
+import reducer from './reducers'; // Import root reducer to manage state updates
 
+// Create Redux store with thunk middleware to enable asychronous actions
+// within action creators
 const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
