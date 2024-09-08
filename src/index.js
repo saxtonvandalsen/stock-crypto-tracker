8b177abf374237574ff8c1f02119bfd0a87910f5
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-09-07 20:29:58 -0700

    Added simple webpage for testing

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import App from './App.js';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root') // Render React app into div with id=root in index.html
+);
