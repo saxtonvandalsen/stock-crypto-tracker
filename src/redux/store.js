49faaee2ce863ea8aad0f3fb1dcb354193df28c9
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-09-07 20:30:38 -0700

    createStore deprecated; added configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducers'; // Import root reducer to manage state updates
 
 // Create Redux store with thunk middleware to enable asychronous actions
 // within action creators
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = configureStore({
+    reducer: reducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
+})
 
 export default store;
