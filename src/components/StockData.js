c8462c18372ce5823261bfc4286c711b9c421c88
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-09-11 14:28:02 -0700

    API returns array, fixed variable to accommodate

--- src/components/StockData.js
+++ src/components/StockData.js
@@ -8,19 +8,22 @@ function StockData() {
     // Hold data returned by API. It's retrieved by global state
     const stockData = useSelector(state => state.stockData);
 
-    if (!stockData) return null;
+    if (!stockData || stockData.length === 0) return null;
+
+    // Accessing array
+    const stock = stockData[0];
 
     // Rendering stock data. stockData info to be displayed
     // if not null
     return (
         <div>
             <h2>Stock Data</h2>
-            <p>Company Name: {stockData.companyName}</p>
-            <p>Company Ticker: {stockData.symbol}</p>
-            <p>Current price: {stockData.price}</p>
-            <p>Market Cap: {stockData.marketCap}</p>
-            <p>Average Volume: {stockData.volAvg}</p>
-            <p>Yearly Range: {stockData.range}</p>
+            <p>Company Name: {stock.companyName}</p>
+            <p>Company Ticker: {stock.symbol}</p>
+            <p>Current price: {stock.price}</p>
+            <p>Market Cap: {stock.marketCap}</p>
+            <p>Average Volume: {stock.volAvg}</p>
+            <p>Yearly Range: {stock.range}</p>
         </div>
     );
 }
