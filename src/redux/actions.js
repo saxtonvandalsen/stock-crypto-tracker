56f669f6bcd631b2d9021f353cab762718602e1f
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-09-10 20:28:38 -0700

    Corrected api syntax error

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,22 +2,32 @@
 export const FETCH_STOCK_DATA = 'FETCH_STOCK_DATA';
 export const FETCH_CRYPTO_DATA = 'FETCH_CRYPTO_DATA';
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 // Action creator for fetching stock data from Company Profile API
 export const fetchStockData = (ticker) => async (dispatch) => {
-    // Asynchronous request to fetch stock data based on provided ticker input
-    const response = await fetch(`https://financialmodelingprep.com/api/v3/profile/${ticker}?apiKey=YOUR_API_KEY`);
-    const data = await response.json();
+   try {
+        // Asynchronous request to fetch stock data based on provided ticker input
+        const response = await fetch(`https://financialmodelingprep.com/api/v3/profile/${ticker}?apikey=${apiKey}`);
+        const data = await response.json();
 
-    // Dispatch action to update Redux state with fetched stock data
-    dispatch({ type: FETCH_STOCK_DATA, payload: data });
+        // Dispatch action to update Redux state with fetched stock data
+        dispatch({ type: FETCH_STOCK_DATA, payload: data });
+   } catch (error) {
+    console.error('Error fetching stock data:', error);
+   }
 };
 
 // Action creator for fetching cryptocurrency data from Full Crypto Quote API
 export const fetchCryptoData = (symbol) => async (dispatch) => {
-    // Asychronous request to fetch crypto data based on provided symbol input
-    const response = await fetch(`https://financialmodelingprep.com/api/v3/quote/${symbol}?apiKey=YOUR_API_KEY`);
-    const data = await response.json();
+   try {
+        // Asychronous request to fetch crypto data based on provided symbol input
+        const response = await fetch(`https://financialmodelingprep.com/api/v3/quote/${symbol}?apikey=${apiKey}`);
+        const data = await response.json();
 
-    // Dispatch action to update Redux state with fetched crypto data
-    dispatch({ type: FETCH_CRYPTO_DATA, payload: data });
+        // Dispatch action to update Redux state with fetched crypto data
+        dispatch({ type: FETCH_CRYPTO_DATA, payload: data });
+   } catch (error) {
+    console.error('Error fetching crypto data:', error);
+   }
 };
