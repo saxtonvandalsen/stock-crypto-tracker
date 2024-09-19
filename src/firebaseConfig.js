7c3f7e19f958071dc39be1371b8b190b8e6db559
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-09-18 18:21:39 -0700

    database syntax editing

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
@@ -6,7 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD18Kf6MgEeaoeKmGKtY_-HwQZZbeGh6Xg",
