2efd8913fc98fe9a80f8aa7e54c280489df6860f
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-09-27 15:37:14 -0700

    edited syntax & self-document code

--- src/components/CryptoForm.js
+++ src/components/CryptoForm.js
@@ -1,25 +1,19 @@
-// Handling user input of crypto coin symbol and dispatching an action to fetch stock data
-
-import React, { useState, useEffect } from 'react'; // Hook to manage local component state
-import { useDispatch, useSelector } from 'react-redux'; // To dispatch actions to the Redux stor
-import { fetchCryptoData } from '../redux/actions'; // Fetching stock data by ticker
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCryptoData } from '../redux/actions';
 
 function CryptoForm() {
 
-    // To update state when input changes
     const [symbol, setSymbol] = useState('');
     const [message, setMessage] = useState(''); // For Displaying feedback messages
     const [messageType, setMessageType] = useState(''); // To differientate success or error messages
 
-    // Accesses dispatch function and used to send actons to Redux store
     const dispatch = useDispatch();
 
-    // Access crypto data via Redux
     const cryptoData = useSelector(state => state.cryptoData);
     const error = useSelector(state => state.error);
 
     useEffect(() => {
-        // If there's stock data and no error, clear the error message
         if (cryptoData && !error) {
             setMessageType('success');
             setMessage('Fetching data for crypto coin.');
@@ -29,27 +23,20 @@ function CryptoForm() {
         }
     }, [cryptoData, error]);
 
-    // Function runs when user presses submit button on form but prevents
-    // the default form behavior (page refresh) and fetchs the stock data
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Check if input is empty or invalid
         if(!symbol.trim()) {
             setMessageType('error');
             setMessage("Please enter a valid crypto coin symbol.");
             return;
         }
 
-        // If valid, dispatch action to fetch crypto data via Redux action
         dispatch(fetchCryptoData(symbol));
 
-        // To clear input field after submission
         setSymbol('');
     };
 
-    // JSX to handle the text input field where the user can type in 
-    // the crypto coin symbol and submit
     return (
         <div>
             <form onSubmit={handleSubmit}>
@@ -68,5 +55,4 @@ function CryptoForm() {
     );
 }
 
-// Available for import and use in other files
 export default CryptoForm;
--- src/components/StockForm.js
+++ src/components/StockForm.js
@@ -1,26 +1,17 @@
-// Handling user input of stock ticker and dispatching an action to fetch stock data from API
-
-import React, { useState, useEffect } from 'react'; // Hook to manage local component state
-import { useDispatch, useSelector } from 'react-redux'; // To dispatch actions to the Redux store
-import { fetchStockData } from '../redux/actions'; // Fetching stock data by ticker
-
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchStockData } from '../redux/actions';
 
 function StockForm() {
-
-    // State to track input and error/success messages
     const [ticker, setTicker] = useState('');
-    const [message, setMessage] = useState(''); // For displaying feedback messages
-    const [messageType, setMessageType] = useState(''); // To differientate success or error messages
+    const [message, setMessage] = useState('');
+    const [messageType, setMessageType] = useState('');
 
-    // Accesses dispatch function and sends actons to Redux store
     const dispatch = useDispatch();
-
-    // Access stock data and error from Redux
     const stockData = useSelector(state => state.stockData);
     const error = useSelector(state => state.error);
 
     useEffect(() => {
-        // If there's stock data and no error, clear the error message
         if (stockData && !error) {
             setMessageType('success');
             setMessage('Fetching data for ticker.');
@@ -30,27 +21,19 @@ function StockForm() {
         }
     }, [stockData, error]);
 
-    // Function runs when user presses submit button on form but prevents
-    // the default form behavior (page refresh) and fetchs the stock data
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Check if input is empty or invalid
-        if(!ticker.trim()) {
+        if (!ticker.trim()) {
             setMessageType('error');
-            setMessage("Please enter a valid stock ticker.");
+            setMessage('Please enter a valid stock ticker.');
             return;
         }
 
-        // If valid, dispatch action to fetch stock data via Redux action
         dispatch(fetchStockData(ticker));
-        
-        // Clear input field after submission
         setTicker('');
     };
 
-    // JSX to handle the text input field where the user can type in 
-    // the stock ticker and submit
     return (
         <div>
             <form onSubmit={handleSubmit}>
